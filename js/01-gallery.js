7b6348a5e9fad91eeed3a9f4600639a2ed24b01e
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-09 16:15:35 +0200

    namber one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,23 +7,48 @@ const galaryEl = document.querySelector('.gallery');
 
 const getTemplateOfPictures = ({ preview, original, description }) =>
     `<div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
+        <a class="gallery__link" href="${original}">
             <img
             class="gallery__image"
             src="${preview}"
-            data-source="large-image.jpg"
+            data-source="${original}"
             alt="${description}"
             />
         </a>
     </div>`;
 
 const render = () => { 
-    // const arrayOfStringOfPictures = galleryItems.map(item => getTemplateOfPictures(item)); - це так було до скорочення!
+    //---Не видаляти!!!--- const arrayOfStringOfPictures = galleryItems.map(item => getTemplateOfPictures(item)); - це так було до скорочення!
     const arrayOfStringOfPictures = galleryItems.map(getTemplateOfPictures);
 
     // console.dir(galaryEl.innerHTML);
     galaryEl.innerHTML = '';
     galaryEl.insertAdjacentHTML('afterbegin', arrayOfStringOfPictures.join(''));
 };
-
 render();
+
+galaryEl.addEventListener('click', onClickOpenModalPicture);
+
+function onClickOpenModalPicture(e) {
+    e.preventDefault();
+    if ( e.target.nodeName !== 'IMG') {
+        return;
+    }
+    // console.dir(e.target.dataset.source);
+    const instance = basicLightbox.create(`
+        <img src="${e.target.dataset.source}" width="800" height="600">
+    `);
+    instance.show();
+    console.dir(instance);
+
+    window.addEventListener('keydown', onEscCloseModal);
+}
+
+function onEscCloseModal(e) {
+    if (e.code !== 'Escape') {
+        return;
+    }
+
+    instance.close();
+    // console.dir(e.code);
+}
