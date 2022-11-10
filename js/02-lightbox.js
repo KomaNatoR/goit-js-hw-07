831b4a551ee5cced878b8619a23c41af39bc2849
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-10 10:41:48 +0200

    start 7-2 hw

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+const listTemplate = (preview, original, description) => `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`;
+
+// listTemplate(galleryItems[0].preview, galleryItems[0].original, galleryItems[0].description);
+
+const render = () => {
+    const arrayFromListTemplate = galleryItems.map(item => listTemplate(item.preview, item.original, item.description));
+
+    listEl.innerHTML = arrayFromListTemplate.join('');
+};
+render();
